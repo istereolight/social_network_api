d2e1eda5995261c7c394121cf357c0e30c3ad7dc
Vitaly Ishchenko <57286101+istereolight@users.noreply.github.com>
2024-07-03 10:53:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const fs = require('fs');
-const cors = require('cors');
-require('dotenv').config()
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var path = require('path');
+var fs = require('fs');
+require('dotenv').config();
 
-
-const app = express();
-
+var app = express();
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// view engine setup
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+// Раздавать статические файлы из папки 'uploads'
+app.use('/uploads', express.static('uploads'));
 
-//Раздаем статические файлы из папки 'uploads'
-app.use('/uploads', express.static('upolads'))
+app.use('/api', require('./routes'));
 
-
-/**
- * http://localhost:3000/api/
- */
-app.use('/api', require('./routes'))
-
-
-if(!fs.existsSync('uploads')) {
-  fs.mkdirSync('uploads')
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
 }
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,7 +35,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page 
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
